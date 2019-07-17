74e0bfd15ff8e02a36e6c9ce450598199519cc96
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-07-17 13:12:53 +0200

    Make responsive
    Closes #1

--- static/script.js
+++ static/script.js
@@ -11,15 +11,18 @@ function generateSubToc (level, headings) {
       var subToc = generateSubToc(level + 1, headings)
       i = i - 1
       if (lastLi !== null) {
-        var button = document.createElement('button')
-        button.textContent = '-'
+        var button = document.createElement('a')
+        button.href="#"
+        button.className = 'collapse'
+        button.textContent = '[-]'
         button.addEventListener('click', (function(toc) {
           return function (e) {
+            e.preventDefault()
             if (toc.style.display === 'none') {
-              this.textContent = '-'
+              this.textContent = '[-]'
               toc.style.display = 'block'
             } else {
-              this.textContent = '+'
+              this.textContent = '[+]'
               toc.style.display = 'none'
             }
           }
@@ -41,7 +44,8 @@ function generateSubToc (level, headings) {
     var a = document.createElement('a')
     a.href = '#' + heading.id
     a.className = 'toc-link-' + heading.tagName.toLowerCase()
-    a.textContent = heading.textContent 
+    a.textContent = heading.textContent
+    a.addEventListener('click', clickToc)
     lastLi.appendChild(a)
     ul.appendChild(lastLi)
   }
@@ -63,12 +67,16 @@ function hasMoreText (node) {
   return node.nextElementSibling.tagName.indexOf('H') !== 0
 }
 
+function getLinkTarget (link) {
+  return decodeURIComponent(link.href.split('#')[1])
+}
+
 function loadExplanation (e) {
   console.log(this.href)
-  var id = decodeURIComponent(this.href.split('#')[1])
+  var id = getLinkTarget(this)
   console.log(id)
   var headingNode = document.getElementById(id)
-  if (!hasMoreText(headingNode)) {
+  if (isSmallScreen || !hasMoreText(headingNode)) {
     // next element is another heading, just jump there
     return true
   }
@@ -87,22 +95,41 @@ function loadExplanation (e) {
   clearAnnotationButton.style.display = 'block'
 }
 
+function clickToc (e) {
+  hideToc()
+}
+
+function hideToc () {
+  document.getElementById('toc-container').classList.remove('show')
+}
+
 var clearAnnotationButton = document.getElementById('clear-annotation')
+var showTocButton = document.getElementById('toc-toggler')
+var isSmallScreen = window.innerWidth < 576
+var isBigScreen = window.innerWidth > 991
+
 
 document.addEventListener('DOMContentLoaded', function () {
   var content = document.getElementById('content')
   var toc = document.getElementById('toc')
   var headings = content.querySelectorAll('h1,h2,h3,h4,h5,h6')
 
-  clearAnnotationButton.addEventListener('click', function(){
+  clearAnnotationButton.addEventListener('click', function () {
     document.getElementById('annotation-content').innerHTML = ''
     clearAnnotationButton.style.display = 'none'
+  })
+  showTocButton.addEventListener('click', function () {
+    var toc = document.getElementById('toc-container')
+    toc.classList.toggle('show')
+    toc.scrollIntoView()
   })
 
   /* Generate TOC */
   headingArray = Array.from(headings)
   var ul = generateSubToc(1, headingArray)
   toc.appendChild(ul)
+  // Unhide toc after
+  document.getElementById('toc-wrapper').style.display = 'block'
 
   /* prepare mapping of headings to explanations and reverse */
   var mapping = {}
@@ -113,6 +140,13 @@ document.addEventListener('DOMContentLoaded', function () {
     if (heading.tagName === 'H1') {
       mainSections.push(i)
     }
+
+    var link = document.createElement('a')
+    link.href = "#" + heading.id
+    link.className = 'self no-print'
+    link.title = "Anker"
+    heading.appendChild(link)
+
     var sectionName = getSectionName(heading)
     if (sectionName === null) {
       continue
@@ -135,13 +169,13 @@ document.addEventListener('DOMContentLoaded', function () {
     if (explanation[sectionName] !== undefined) {
       var link = document.createElement('a')
       link.href = "#" + explanation[sectionName].id
-      link.className = 'ref begruendung'
+      link.className = 'ref begruendung no-print'
       link.title = "Zur Begründung"
 
       if (hasMoreText(explanation[sectionName])) {
         link.textContent = 'Begründung'
       } else {
-        link.innerHTML = '&rarr;'
+        link.innerHTML = '&darr;'
       }
 
       link.addEventListener('click', loadExplanation)
@@ -160,9 +194,9 @@ document.addEventListener('DOMContentLoaded', function () {
     if (mapping[sectionName] !== undefined) {
       var link = document.createElement('a')
       link.href = "#" + mapping[sectionName].id
-      link.className = 'ref lawsection'
+      link.className = 'ref lawsection no-print'
       link.title = "Zur Stelle im Gesetz"
-      link.innerHTML = '&rarr;'
+      link.innerHTML = '&uarr;'
       heading.appendChild(link)
     }
   }
